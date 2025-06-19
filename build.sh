5afa57b605c2bcdb5424a49c226dcdb8f34a978e
Bhargav Rathod <bhargavrathod98@gmail.com>
2025-06-19 08:44:29 +0530

    config changes

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-pip install --upgrade pip setuptools wheel
-pip install -r requirements.txt 
+# build.sh
+python -m pip install --upgrade pip
+pip install setuptools==65.5.0 wheel==0.38.4
+pip install -r requirements.txt
